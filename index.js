e1b553a5ff45b47af16d7bed2e31eed2f332f954
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2022-08-04 12:02:40 +0530

    initial commit
    added database connectivity

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = process.env.PORT || 80;
+
+app.use(cors());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+
+// Database Connection
+const connection = require("./utils/database");
+connection.on("error", console.error.bind(console, "connection error: "));
+connection.once("open", function () {
+    console.log("Database Connected successfully");
+})
+
+// Starting App
+app.listen(port, function(){
+    console.log("App is running at port " + port);
+})
