038c815d893d0563fe0a2ab38f7297d8a4eb1535
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2022-08-18 15:58:40 +0530

    added event route

---
+++ routes/eventRoutes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authControllers")
+const eventControllers = require("../controllers/eventControllers")
+
+router.post("/", authController.userVerification, createEvent);
+router.delete("/", authController.userVerification, deleteEvent);
+router.get("/", authController.userVerification, getEvent);
+
+function createEvent(req, res, next){
+    eventControllers.creatEvent(req.body)
+        .then( data => {
+            res.status(data.status).send(data);
+        })
+}
+
+function deleteEvent(req, res, next){
+    eventControllers.deleteEvent(req.body)
+        .then( data => {
+            res.status(data.status).send(data);
+        })
+}
+
+function getEvent(req, res, next){
+    if(req.body.eventID){
+        eventControllers.getEvent(req.body)
+            .then( data => {
+                res.status(data.status).send(data);
+            })
+    } else {
+        eventControllers.getAllEvent(req.body)
+        .then( data => {
+            res.status(data.status).send(data);
+        })
+    }
+}
+
+module.exports = router;
